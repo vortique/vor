9f5b9d103e946ea0d4c51e8b712265a8a23a2785
Badi <wayw04321@gmail.com>
2025-08-16 18:08:37 +0300

    Add project files.

---
+++ Vor/Vor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Vor
+{
+    internal class Vor
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                String filePath = args[0];
+
+                if (File.Exists(args[0]))
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(filePath))
+                        {
+                            String line;
+
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                Console.WriteLine(line);
+                            }
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine("File doesn't exists! Please ensure file's location.");
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        Console.WriteLine("File's directory doesn't exists! Please ensure file's location.");
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("Something went wrong. " + e);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File doesn't exists! Please ensure file's location.");
+                }
+            }
+        }
+    }
+}
